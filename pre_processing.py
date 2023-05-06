0b8915e85c6ccf23f67f9d776d3111c05cdbc14e
Raaulsthub <rsteinmetz@inf.ufsm.br>
2023-05-06 15:06:59 -0300

    cleaning

--- pre_processing.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-
-def gaussian(img, ksize=(5, 5)):
-    # Apply Gaussian filter
-    sigmaX = 0      # Standard deviation in X direction (0 means it is computed from ksize)
-    sigmaY = 0      # Standard deviation in Y direction (0 means it is computed from ksize)
-    return cv2.GaussianBlur(img, ksize, sigmaX, sigmaY)
